5004795c49db9cff5c05ca21796f02a3a20fe453
acapr <acapr@Cooper_inspo16.dhcp.wustl.edu>
2024-10-03 11:16:57 -0500

    flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,23 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+			   StdDraw.clear();
+			   StdDraw.setXscale(0, 10);
+			   StdDraw.setYscale(0, 10);
+			   StdDraw.setPenColor(255, 255, 255);
+			   StdDraw.filledRectangle(5,0,2,10);
+			   StdDraw.setPenColor(255, 155, 0);
+			   StdDraw.filledCircle(5,5,2);
+			   StdDraw.setPenColor(255, 200, 0);
+			   StdDraw.filledCircle(5,5,1.5);
+			   StdDraw.setPenColor(255, 255, 0);
+			   StdDraw.filledCircle(5,5,1);
+			   StdDraw.setPenColor(100, 100, 255);
+			   StdDraw.filledRectangle(0,0,10,2);
+			   StdDraw.setPenColor(0, 255, 100);
+			   StdDraw.filledRectangle(0,9,10,1);
+			   StdDraw.setPenColor(255, 0, 0);
+			   StdDraw.filledRectangle(0,0,3,10);
+			   StdDraw.filledRectangle(9,0,2,10);
+			}	
 	}
-}
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,16 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		int[] color = new int[3];
+		String shape = in.nextLine();
+		for (int i=0;i < 3; i++) {
+			color[i] = in.nextInt();
+		}
+		double[] perameters = new double[3];
+		boolean filled = in.nextBoolean();
+		for (int i=0;i < 3; i++) {
+			perameters[i] = in.nextDouble();
+		}
 		
 	}
 }
